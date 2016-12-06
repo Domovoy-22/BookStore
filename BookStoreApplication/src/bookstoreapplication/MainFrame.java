eea78d442994a32c7877bd4e7cbdd1cbbbd3447b
Fedyaev Vyacheslav <Gitara-12@mail.ru>
2016-12-06 21:06:45 +0600

    Add new form "KadrForm"

--- BookStoreApplication/src/bookstoreapplication/MainFrame.java
+++ BookStoreApplication/src/bookstoreapplication/MainFrame.java
@@ -63,6 +63,11 @@ public class MainFrame extends javax.swing.JFrame {
         jButton3.setBackground(java.awt.SystemColor.inactiveCaption);
         jButton3.setFont(new java.awt.Font("Comic Sans MS", 3, 18)); // NOI18N
         jButton3.setText("Магазин");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setBackground(java.awt.SystemColor.inactiveCaption);
         jButton4.setFont(new java.awt.Font("Comic Sans MS", 3, 18)); // NOI18N
@@ -153,6 +158,10 @@ public class MainFrame extends javax.swing.JFrame {
        StockForm stock = new StockForm();
        stock.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
